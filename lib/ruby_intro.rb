702bc3735584c27d4cbebb97cee7c0db5d55776a
Héctor Salazar <h.salazar509@gmail.com>
2015-10-30 05:41:43 +0000

    Resueltas parte 1, y 2/3 de la parte 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,25 +3,47 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.reduce(0, :+)  
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.sort.last(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  # It first evaluates it is actually 0
+  if arr.empty?
+    return true if n == 0
+  else
+    
+    # array.combination(n).to_a permutes a number combinations possible,
+    # given each combination contains 'n' members.
+    #
+    # EXAMPLE
+    # a = [1, 2, 3]
+    #
+    # a.combination(1).to_a => [[1], [2], [3]]
+    # a.combination(2).to_a => [[1,2], [1,3], [2,3]]
+    # a.combination(3).to_a => [[1,2,3]]
+    
+    arr.combination(2).to_a.each do |combi|
+      
+      # If the sum of a given combination totals n, then return true
+      return true if sum(combi) == n
+    end
+  end
+  
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  puts "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ %r{\A(?=[^aeiou])(?=[a-z])}i
 end
 
 def binary_multiple_of_4? s
